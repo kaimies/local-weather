36de39ba500a6f50f7a03ebe05d65b990d999be5
Kai Mies <develop@regnavi.de>
2017-05-17 07:39:04 +0200

    Add user location fetching

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import { Container, Segment, Header, Button, Icon } from 'semantic-ui-react';
 import './App.css';
 
 class App extends Component {
+  getUserLoation() {
+    return new Promise((resolve, reject) => {
+      if (!("geolocation" in navigator)) {
+        return reject(new Error('Geolocation not supported'));
+      }
+
+      navigator.getCurrentPosition((position) => {
+        return resolve(position.coords);
+      });
+    });
+  }
+
   render() {
     return (
       <Container>
