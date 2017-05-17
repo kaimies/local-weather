b61a474d0ed5f49639a601ce54062728f8ee72c5
Kai Mies <develop@regnavi.de>
2017-05-17 11:33:03 +0200

    Add source code and dark sky buttons

--- src/App.js
+++ src/App.js
@@ -21,6 +21,8 @@ class App extends Component {
         <Header as="h1" textAlign="center">Weatcher App</Header>
         <Segment>
         </Segment>
+        <Button basic as="a" href="https://darksky.net/poweredby/" target="_blank" rel="noopener noreferrer">Powered by Dark Sky</Button>
+        <Button basic floated="right" as="a" href="https://github.com/kaimies/local-weather" target="_blank" rel="noopener noreferrer">Source Code</Button>
       </Container>
     );
   }
