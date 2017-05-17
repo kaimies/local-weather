8cd64c17e318aae710643169b8af0d84d7d0fe3a
Kai Mies <develop@regnavi.de>
2017-05-17 07:32:49 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Container, Segment, Header, Button, Icon } from 'semantic-ui-react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Container>
+        <Header as="h1" textAlign="center">Weatcher App</Header>
+        <Segment>
+        </Segment>
+      </Container>
+    );
+  }
+}
+
+export default App;
